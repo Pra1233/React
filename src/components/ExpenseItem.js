443cabdbf500b693d20eba14f8d9140e8a4fe992
Prabhat <103235389+Pra1233@users.noreply.github.com>
2023-09-30 21:42:46 +0530

    Task2

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,19 @@
-import React from "react";
+import "./ExpenseItem.css";
 
 const ExpenseItem = () => {
+  const expenseDate = new Date(2023, 10, 1);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 200;
+  const LocationOfExpenditure = "Mumbai";
+
   return (
-    <div>
-      <h2>Expense Items</h2>
-      <h3>Food Rs 10</h3>
-      <h3>Petrol Rs 100</h3>
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <h2>{LocationOfExpenditure}</h2>
+        <div className="expense-item__price">Rs{expenseAmount}</div>
+      </div>
     </div>
   );
 };
