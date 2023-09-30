e7f13ab86dffd4616be645b81f39d47c4fd80b42
Prabhat <103235389+Pra1233@users.noreply.github.com>
2023-09-30 20:59:46 +0530

    Lets start coding
    Adding expense hardcoded

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <h2>Hello</h2>
+      <ExpenseItem />
+    </div>
+  );
+}
+
+export default App;
