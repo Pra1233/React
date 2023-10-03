90e1685c3202f58967557ff579cca29ededdccec
Prabhat <103235389+Pra1233@users.noreply.github.com>
2023-10-04 00:53:49 +0530

    JSX limitation and solution
    JSX limitation and solution

--- src/components/CourseGoals/CourseGoalList/CourseGoalList.js
+++ src/components/CourseGoals/CourseGoalList/CourseGoalList.js
@@ -2,6 +2,26 @@ import React from "react";
 
 import CourseGoalItem from "../CourseGoalItem/CourseGoalItem";
 import "./CourseGoalList.css";
+// import ReactDOM from "react-dom";
+// const Backdrop = (props) => {
+//   return <div onClick={props.onConfirm} />;
+// };
+
+// const ModalOverlay = (props) => {
+//   return (
+//     <ul className="goal-list">
+//       {props.items.map((goal) => (
+//         <CourseGoalItem
+//           key={goal.id}
+//           id={goal.id}
+//           onDelete={props.onDeleteItem} //course GoalItems
+//         >
+//           {goal.text}
+//         </CourseGoalItem>
+//       ))}
+//     </ul>
+//   );
+// };
 
 const CourseGoalList = (props) => {
   return (
@@ -19,4 +39,19 @@ const CourseGoalList = (props) => {
   );
 };
 
+// const CourseGoalList = (props) => {
+//   return (
+//     <>
+//       {ReactDOM.createPortal(
+//         <Backdrop onClick={props.onConfirm} />,
+//         document.getElementById("backdrop-root")
+//       )}
+
+//       {ReactDOM.createPortal(
+//         <ModalOverlay />,
+//         document.getElementById("overlay-root")
+//       )}
+//     </>
+//   );
+// };
 export default CourseGoalList;
